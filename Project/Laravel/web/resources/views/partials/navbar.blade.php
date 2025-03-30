fa48d3c252d7cb36870a7c7203ae302212d7c92a
Steve Rodarte <steve.julian13@hotmail.com>
2025-03-30 02:18:51 -0600

    Fix de CSS y Hipervinculos

--- Project/Laravel/web/resources/views/partials/navbar.blade.php
+++ Project/Laravel/web/resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar">
-    <div class="logo">MyLogo</div>
+    <div class="logo" onclick="window.location.href='{{ route('home') }}'">MyLogo</div>
     <ul class="nav-links">
         <li><a href="#">SHOP</a></li>
         <li><a href="#">NEWS</a></li>
@@ -8,7 +8,7 @@
     </ul>
     <div class="hamburger" onclick="toggleMenu()">&#9776;</div>
     <div class="button-container">
-    <button class="button button1" onclick="register()">NEW ACCOUNT</button>
+    <button class="button button1" onclick="window.location.href='{{ route('register') }}'">NEW ACCOUNT</button>
     <button class="button button2" onclick="window.location.href='{{ route('login') }}'">LOG IN</button>
     </div>
 </nav>
